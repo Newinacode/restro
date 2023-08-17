bc51c961696fbbaa0a178c99e0f941e566df9413
Blanks0O7 <radhesubedi007@gmail.com>
2023-08-17 11:03:23 +0545

    index and setting fixed

--- restro/settings.py
+++ restro/settings.py
@@ -77,32 +77,32 @@ WSGI_APPLICATION = 'restro.wsgi.application'
 
 
 
-# dbname = config('NAME')
-# dbpassword = config('PASSWORD')
-# dbhost = config('HOST')
-# dbport = config('PORT')
-# dbuser = config('USERNAME')
+dbname = config('NAME')
+dbpassword = config('PASSWORD')
+dbhost = config('HOST')
+dbport = config('PORT')
+dbuser = config('USERNAME')
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': dbname,
-#         'USER': dbuser,
-#         'PASSWORD': dbpassword,
-#         'HOST': dbhost,
-#         'PORT': dbport,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': dbname,
+        'USER': dbuser,
+        'PASSWORD': dbpassword,
+        'HOST': dbhost,
+        'PORT': dbport,
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
